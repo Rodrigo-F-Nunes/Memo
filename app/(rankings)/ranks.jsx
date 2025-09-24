8fa1ac8676d926b906de9b168887f388f09f9425
Rodrigo-F-Nunes <odeioestargripado@gmail.com>
2025-09-24 19:44:29 -0300

    Add files via upload

---
+++ app/(rankings)/ranks.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, View } from "react-native";
+
+const rankDetails = () => {
+  return (
+    <View style={styles.container}>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    fontFamily: "Poppins-Bold",
+  },
+});
+
+export default rankDetails;
